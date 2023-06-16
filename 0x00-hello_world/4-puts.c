4e69a9fd6c02a16a569c9b25f6369231ce5f8639
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-15 18:45:32 -0700

    Updated betty, main and return warning

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * put will print a string
- * return always void
+ * Description: 'prints a string
+ * Return: Always 0 success
  */
 int main(void)
 {
