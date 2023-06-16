81be021099dd665893fd9a215c9775e232c85eec
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-15 18:38:33 -0700

    using betty and gcc to compile a .c file to run string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * put will print a string
+ * return always void
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
