3aa622bc813ce546618777d2be02016d279a3a91
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-19 14:56:36 -0700

    1-last_digit.c prints last value and and checks the last digit in if statements

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+* main - Entry Point
+* description: 'get the last digit of the number.'
+* Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int n;
+	int rm;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	rm = n % 10;
+	if (rm > 5)
+	{
+		printf("Last digit of %d is %d  greater than 5\n", n, rm);
+	}
+	else if (rm == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, rm);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rm);
+	}
+	return (0);
+}
