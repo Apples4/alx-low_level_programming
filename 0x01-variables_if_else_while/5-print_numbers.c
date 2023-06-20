407e8bd81d1a118995974d312e39d01abd84f231
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-19 17:30:00 -0700

    5-print_numbers.c printing base 10 numbers using a for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: "Printing base 10 single digits"
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+return (0);
+}
