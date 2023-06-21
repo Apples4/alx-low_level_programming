62ab82d6a7c333b470f9942dd5fee1deafdd7991
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-21 09:53:15 -0700

    8-24_hours.c updated the loop and moved the if statement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,31 +7,27 @@
  */
 void jack_bauer(void)
 {
-	int x;
-	int y;
-	int i;
-	int j;
+	int x, y, i, j;
 
 	for (x = 0; x <= 2; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
+			if ((x <= 1 && y <= 9) || (x <= 2 && y <= 3))
+			{
 			for (i = 0; i <= 5; i++)
 			{
 				for (j = 0; j <= 9; j++)
 				{
-					if (x >= 2 && y >= 4)
-					{
-						break;
-						_putchar(x + 48);
-						_putchar(y + 48);
-						_putchar(58);
-						_putchar(i + 48);
-						_putchar(j + 48);
-						_putchar('\n');
-					}
+					_putchar(x + 48);
+					_putchar(y + 48);
+					_putchar(58);
+					_putchar(i + 48);
+					_putchar(j + 48);
+					_putchar('\n');
 				}
 			}
 		}
+	}
 	}
 }
