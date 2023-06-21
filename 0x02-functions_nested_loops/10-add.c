f6f1dbfc61c23b1c7a581b81a811c747dde7c230
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-21 13:52:55 -0700

    10-add.c creating add function for two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two inegers
+ * @x: first integer for addition
+ * @y: second integer for addition
+ * Return: Always integer value
+ */
+
+int add(int x, int y)
+{
+	int z = x + y;
+
+	return (z);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - multplication tables using loops
+ *
+ * Return: no return
+ */
+void times_table(void)
+{
+	int i, j, x, y, z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			x = i * j;
+			if (x > 9)
+			{
+				y = x % 10;
+				z = (x - y) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(z + '0');
+				_putchar(y + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(x + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
+
+
