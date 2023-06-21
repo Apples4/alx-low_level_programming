6b5af7064548fdec5c3b8f5c6204d78006c4c9a1
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-20 20:53:45 -0700

    5-sign.c a function that can print a number and sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - shows the sign of integer
+ * Descrpition: function prints 1 for > 0, 0 == 0
+ * and -1 if "
+ *
+ * @n: input integer as a number for the function
+ * Return: 1 if input > 1 and
+ * input 0 if 0 == 0 and -1 for other
+ */
+int print_sign(int n)
+{
+	if (n > 1)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar(45);
+		return (-1);
+	}
+	_putchar('\n');
+}
