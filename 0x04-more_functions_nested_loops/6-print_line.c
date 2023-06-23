ab540229af6d03ca019396dc1ebfdaf36bcd48e6
Apples4 <nhlanhla.mavuka@gmail.com>
2023-06-23 08:24:29 -0700

    6-print_line.c prints a line based on the input integer value

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line uinsg _ based on a number given
+ * @n: is an intger input
+ * return: is a printout of _
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+_putchar('\n');
+}
+
