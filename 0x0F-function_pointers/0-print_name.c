d5f12ef5d2a621a35a9805d1236794bdbb873d05
Apples4 <nhlanhla.mavuka@gmail.com>
2023-07-19 10:57:45 -0700

    0-print_name.c, made a function that has a function pointer to take an input name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: input char, name of person
+ * @f: pointer of a function
+ * Return: Always 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if  (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
