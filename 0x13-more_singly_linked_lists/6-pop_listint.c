d4dc544bba19d39780abc9a68a8bfb1e141830e4
Apples4 <nhlanhla.mavuka@gmail.com>
2023-08-02 01:15:10 +0200

    6-pop_listint.c  fixed betty errors

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * pop_listint - function that deletes the head node and returns and returns the element
+ * pop_listint - function that deletes the head node and returns a node
  * @head: input pointer of list
  * Return: int of pointer data
  */
 int pop_listint(listint_t **head)
 {
 	listint_t *node_1;
-	
+
 	if (head == NULL)
 	{
 		return (0);
@@ -17,7 +17,7 @@ int pop_listint(listint_t **head)
 	{
 		return ((*head)->n);
 		node_1 = *head;
-		free (node_1);
+		free(node_1);
 	}
 }
 
