eacf03c83dfb8f7b77f1de6988b5e154cf487454
Apples4 <nhlanhla.mavuka@gmail.com>
2023-11-03 19:12:31 +0200

    updated task 3

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		temp = ht->array[i];
 		while (temp != NULL)
 		{
-			printf("%s'%s' : '%s'", comma, temp->key, temp->value);
+			printf("%s'%s': '%s'", comma, temp->key, temp->value);
 			comma = ", ";
 			temp = temp->next;
 		}
