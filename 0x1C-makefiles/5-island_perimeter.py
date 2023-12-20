0659c3ebc3641c864c60821df287a653f9a2847d
Apples4 <nhlanhla.mavuka@gmail.com>
2023-12-21 01:03:11 +0200

    adding files to repo

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" function that describes a island grid """
+
+
+def island_perimeter(grid):
+    """
+    args:
+        grid - matrix input
+
+    return:
+        the perimeter of the island
+    """
+    per = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                per += 4
+                if row > 0 and grid[row -1][column] == 1:
+                    per -= 1
+                if column > 0 and grid[row][column-1] == 1:
+                    per -= 1
+                if column < (len(grid[row])-1) and grid[row][column+1] == 1:
+                    per -= 1
+                if row < (len(grid)-1) and grid[row+1][column] == 1:
+                    per -= 1
+    return (per)
+
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,11 @@
+This repos is to illustrate the use of makefile and using it to automate compliation in C
+
+# Aim:
+What are make, Makefiles
+When, why and how to use Makefiles
+What are rules and how to set and use them
+What are explicit and implicit rules
+What are the most common / useful rules
+What are variables and how to set and use them
+
+There are **6** Mandatory tasks
